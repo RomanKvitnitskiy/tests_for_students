02a2e94c118eabe716267bc514f6f0845b573f66
RomanKvitnitskiy <kvitnitskiy2002@gmail.com>
2020-12-17 13:57:32 +0200

    ready Student.class

--- tests_for_students/Student.cpp
+++ tests_for_students/Student.cpp
@@ -1,1 +1,18 @@
 #include "Student.h"
+
+Student::Student() 
+{
+	name = "noname";
+	group = "unknow";
+	course = NULL;
+}
+Student::Student(string a, string b, int c) : name(a), group(b), course(c) {}
+
+void Student::setName(string name) { this->name = name; }
+string Student::getName() { return name; }
+
+void Student::setGroup(string group) { this->group = group; }
+string Student::getGroup() { return group; }
+
+void Student::setCourse(int course) { this->course = course; }
+int Student::getCourse() { return course; }
--- tests_for_students/Student.h
+++ tests_for_students/Student.h
@@ -6,14 +6,18 @@ class Student
 {
 public:
 	Student();
-	Student();
+	Student(string, string, int);
 
 	void setName(string);
 	string getName();
 
+	void setGroup(string);
+	string getGroup();
 
+	void setCourse(int);
+	int getCourse();
 protected:
-	string name;
+	string name; 
 	string group;
 	int course;
 };
